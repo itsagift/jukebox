6954e589a9e07e4d0d126c0e6cb4308438c07975
itsagift <30632225+itsagift@users.noreply.github.com>
2024-11-22 14:25:45 -0500

    Media player plays music on start

--- hellofx/SongPlayer.java
+++ hellofx/SongPlayer.java
@@ -6,14 +6,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javafx.scene.Group;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
 public class SongPlayer extends Application {
+
+    private static final String MEDIA_URL =
+    "file:/Users/aa/Documents/JokerOST.mp3";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        // Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        // primaryStage.setTitle("Embedded Media Player");
+        Group root = new Group();
+        
+        Scene scene = new Scene(root, 540, 210);
+
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
         primaryStage.show();
+        Media media = new Media(MEDIA_URL);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+        MediaView mediaView = new MediaView(mediaPlayer);
+        ((Group)scene.getRoot()).getChildren().add(mediaView);
     }
 
 
